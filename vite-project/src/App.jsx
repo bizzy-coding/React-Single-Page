ad92b19ee385662c1fca6f2f0537f0797ddd9881
Bizzy-coding <bizzycoding@gmail.com>
2023-04-09 11:06:11 +0100

    setting up folder structure

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "./Components/Main";
+import Navbar from "./Components/Navbar";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
